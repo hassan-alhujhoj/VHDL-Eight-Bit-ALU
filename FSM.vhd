dfa6d9ce741cda7b53bae489517d90960e1b2600
Hassan Ali Alhujhoj <haa61@uclive.ac.nz>
2019-05-11 00:03:02 +1200

    changes to modules

---
+++ FSM.vhd
@@ -0,0 +1,53 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+-- Simple FSM moudle that changes the its state. There are two states: 
+-- A = enter the value of the register 0,
+-- B = enter the value of register 1, 
+entity FSM is
+	port(A, B		: in STD_LOGIC_VECTOR(7 downto 0);
+		buttonC		: in STD_LOGIC;
+		reset, clk	: in STD_LOGIC;
+		operandA, operandB: out STD_LOGIC_VECTOR(7 downto 0));
+end FSM;
+
+Architecture behaviour of FSM is
+	
+	type state_type is (s0, s1); 	-- two state machine
+	signal current_s, next_s : state_type; 	-- current and next state declaration
+	
+	begin
+	
+	process
+		begin
+			if reset = '1' then
+				current_s <= s0;
+			elsif rising_edge(clk) then
+				current_s <= next_s;
+			end if;
+	end process;
+	
+	--implement the finite state machine
+	process(current_s, A, B)
+		begin
+			case current_s is
+				when s0 => 				-- when current state is "s0"
+					if(buttonC'event and buttonC = '0') then
+						operandA <= A;
+						next_s <= s0;	-- only count when buttonC is high
+					elsif (buttonC'event and buttonC = '1') then
+						operandB <= B;
+						next_s <= s1;
+					end if;
+					
+				when s1 => 				-- when current state is "s1"
+					if(buttonC'event and buttonC = '0') then
+						operandB <= B;
+						next_s <= s1;	-- only count when buttonC is high
+					elsif (buttonC'event and buttonC = '1') then
+						operandA <= A;
+						next_s <= s0;
+					end if;
+            end case;
+	end process;
+end behaviour;
--- main.vhd
+++ main.vhd
@@ -9,50 +9,57 @@ ENTITY main IS
 		SW 								: in STD_LOGIC_VECTOR(15 downto 0);
 		LED 							: in STD_LOGIC_VECTOR(15 downto 0);
 		BTNC, BTNU, BTND, BTNR, BTNL 	: in STD_LOGIC;
-		opcode							: in STD_LOGIC_VECTOR(3 downto 0);
+		opcode							: in STD_LOGIC_VECTOR(2 downto 0);
 		dataBus 						: in STD_LOGIC_VECTOR(7 downto 0);
 		output 							: out STD_LOGIC_VECTOR(7 downto 0));
 END main;
 
 ARCHITECTURE BEHAVIOUR OF main 
 
-	component oprandA is
-		port();
-	end component;
-
-	component operandB is
-		port();
-	end component;
-
-	component reg0 is 
-		port();
-	end component;
-
-	component reg1 is 
-		port();
-	end component;
-
 	component ALU is 
-		port();
-	end component;
-
-	component display is 
-		port();
+	   port (operation : in std_logic_vector (1 downto 0);
+			A, B 	: in std_logic_vector(7 downto 0);
+			result 	: out std_logic_vector(7 downto 0));
 	end component;
 
 	component FSM is 
-		port();
+		port(A, B		: in STD_LOGIC_VECTOR(7 downto 0);
+			C			: in STD_LOGIC_VECTOR(1 downto 0);
+			buttonC		: in STD_LOGIC;
+			reset, clk	: in STD_LOGIC;
+			FSMout		: out STD_LOGIC);
 	end component;
 
-	component counter is
-		port();
+	component upButton is
+		port(buttonU: in STD_LOGIC;
+			opcode: out STD_LOGIC_VECTOR(1 downto 0));
 	end component;
+	
+		component rightButton is
+		port(buttonR: in STD_LOGIC;
+			opcode: out STD_LOGIC_VECTOR(1 downto 0));
+	end component;
+	
+		component downButton is
+		port(buttonD: in STD_LOGIC;
+			opcode: out STD_LOGIC_VECTOR(1 downto 0));
+	end component;
+	
+		component leftButton is
+		port(buttonL: in STD_LOGIC;
+			opcode: out STD_LOGIC_VECTOR(1 downto 0));
+	end component;
+	
+	signal reg0 : STD_LOGIC_VECTOR(7 downto 0);
+	signal reg1 : STD_LOGIC_VECTOR(7 downto 0);
+	signal opcode : STD_LOGIC_VECTOR(1 downto 0);
 
 	BEGIN
-	
-	opcode <= SW(15) & SW(14) & SW(13) & SW(12); -- This is the 4-bit opcode
-	process()
-		begin
-	end process;
-	
+		process()
+			begin
+		end process;
+		
+		U1: FSM
+			port map (reg0 => A, reg1 => B, opcode => C, BTNC => buttonC, CLK100MHZ => clk);
+		
 END BEHAVIOUR;
