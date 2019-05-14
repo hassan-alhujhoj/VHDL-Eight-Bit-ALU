8675818ba29939fb9509f9e67a510297593a56bc
Hassan Ali Alhujhoj <haa61@uclive.ac.nz>
2019-05-14 18:50:24 +1200

    Added missing and updated files

--- seg7.vhd
+++ seg7.vhd
@@ -1,27 +1,29 @@
-LIBRARY ieee;
-USE ieee.std_logic_1164.all;
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
 
-ENTITY seg7 IS 
-	PORT(bcd : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
-		led : OUT STD_LOGIC_VECTOR(6 DOWNTO 0));
-END seg7;
+entity seg7 is
+    Port (bcd       : in STD_LOGIC_VECTOR (3 downto 0);
+          seg7_out    : out STD_LOGIC_VECTOR(6 downto 0));
+end seg7;
 
-ARCHITECTURE BEHAVIOUR OF seg7 IS 
-BEGIN
-	PROCESS(bcd)
-	BEGIN
-		CASE bcd IS				--"abcdefg"
-			WHEN "0000" => led <= "0000001";
-			WHEN "0001" => led <= "1001111";
-			WHEN "0010" => led <= "0010010";
-			WHEN "0011" => led <= "0000110";
-			WHEN "0100" => led <= "1001100";
-			WHEN "0101" => led <= "0100100";
-			WHEN "0110" => led <= "0100000";
-			WHEN "0111" => led <= "0001111";
-			WHEN "1000" => led <= "0000000";
-			WHEN "1001" => led <= "0001100";
-			WHEN OTHERS => led <= "-------";
-		END CASE;
-	END PROCESS;
-END BEHAVIOUR;
+architecture Behavioral of seg7 is
+    signal segments_out : STD_LOGIC_VECTOR (6 downto 0);
+    begin
+        seven_segment_process: process(bcd)
+            begin
+                case bcd is                      --"abcdefg"
+                    when "0000" => segments_out <= "0000001";   --0
+                    when "0001" => segments_out <= "1001111";   --1
+                    when "0010" => segments_out <= "0010010";   --2
+                    when "0011" => segments_out <= "0000110";   --3
+                    when "0100" => segments_out <= "1001100";   --4
+                    when "0101" => segments_out <= "0100100";   --5
+                    when "0110" => segments_out <= "0100000";   --6
+                    when "0111" => segments_out <= "0001111";   --7
+                    when "1000" => segments_out <= "0000000";   --8
+                    when "1001" => segments_out <= "0000100";   --9
+                    when others => segments_out <= "1111111";   
+                end case;
+            end process seven_segment_process; 
+    seg7_out <= segments_out;
+end Behavioral;
