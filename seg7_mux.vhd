a05eb431eaab4153417c13e23dce13d9f663fe91
Hassan Ali Alhujhoj <haa61@uclive.ac.nz>
2019-05-24 17:10:45 +1200

    seg7_mux.vhd header added

--- seg7_mux.vhd
+++ seg7_mux.vhd
@@ -1,3 +1,20 @@
+----------------------------------------------------------------------------------
+-- Academic Institution: University of Canterbury. 
+-- Engineer: Hassan Alhujhoj (haa61), Jin Su Kim (jsk71), Abdullah Naeem (ana104)
+-- 
+-- Create Date: 22.05.2019 10:49:33
+-- Design Name: VHDL programming
+-- Module Name: seg7_mux - behavioural
+-- Project Name: ALU + FSM + REG Project
+-- Target Devices: Nexys 4 DDR Artix-7 FPGA
+-- Tool Versions: VHDL Vivado
+-- Description: This module is mux that selects the the digit of the 8-bit operand. The 8-bit operand can go up to 255, for example,
+-- where 2 is digit1, 5 is digit2, and 5 is digit3. Futhermore, this mux select the anodes and cathodes.
+-- 
+-- Revision: Code has been revised by all three designers, Hassan A, Jin K, and Abdullah N (24/05/2019).
+
+----------------------------------------------------------------------------------
+
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_ARITH.ALL;
@@ -13,7 +30,7 @@ entity seg7_mux is
 		cathode_out	: out std_logic_vector(6 downto 0));
 end seg7_mux;
 
-architecture behaviour of seg7_mux is
+architecture behavioural of seg7_mux is
 	begin
 		process(clk, digit1, digit2, digit3)
 			variable counter : INTEGER := 0;
@@ -34,4 +51,4 @@ architecture behaviour of seg7_mux is
                     end if;
                 end if;
 		end process;				
-end behaviour;
+end behavioural;
